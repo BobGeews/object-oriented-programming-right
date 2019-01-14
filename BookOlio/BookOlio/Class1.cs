c1d96df55748ac7cb474b4cc00b0805dc17ae723
BobGeews <ossi.hakkinen@hotmail.fi>
2019-01-14 08:24:36 +0200

    Book software done.

---
+++ BookOlio/BookOlio/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookOlio
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public string idNum;
+        public int price;
+
+        public Book(string title, string author, string idNum, int price)
+        {
+            this.title = title;
+            this.author = author;
+            this.idNum = idNum;
+            this.price = price;
+            
+        }
+        public void PrintThings()
+        {
+            Console.WriteLine($"Kirjan nimi: {this.title}");
+            Console.WriteLine($"Kirjoittaja: {this.author}");
+            Console.WriteLine($"ID: {this.idNum}");
+            Console.WriteLine($"Hinta: {this.price}e");
+        }
+        public string CompareBook(Book book)
+        {
+            string text = "";
+
+            if (this.price > book.price)
+            {
+                text = $"{this.title} on kalliimpi kuin {book.title} kirja";
+            }
+            else
+            {
+                text = $"{book.title} on kalliimpi kuin {this.title} kirja";
+            }
+            return text;
+
+        }
+       
+    }
+}
---
+++ BookOlio/BookOlio/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace BookOlio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string choise = null;
+            
+            Console.WriteLine("**KIRJATEHTÄVÄ**");
+
+            //Book LOTRO = new Book("Kunkun Paluu", "JumskiLAVIS", "12345", 10);
+            //LOTRO.PrintThings();
+
+            //Console.WriteLine("---------------------------------------------------------");
+
+            //Book NARNIA = new Book("Keittokirja", "Soppamies", "11111", 5);
+            //NARNIA.PrintThings();
+
+            //Console.WriteLine(LOTRO.CompareBook(NARNIA));
+            do
+            {
+                choise = UserInterface();
+                switch (choise.ToUpper())
+                {
+                    case "Q":
+                        {
+                            Console.WriteLine("---------------------------------------------------------");
+                            Book LOTRO = new Book("Kunkun Paluu", "JumskiLAVIS", "12345", 10);
+                            LOTRO.PrintThings();
+
+                            Console.WriteLine("---------------------------------------------------------");
+
+                            Book NARNIA = new Book("Keittokirja", "Soppamies", "11111", 5);
+                            NARNIA.PrintThings();
+                            Console.WriteLine("---------------------------------------------------------");
+                            break;
+                        }
+                        
+                    case "W":
+                        {
+                            Console.WriteLine("---------------------------------------------------------");
+                            Book LOTRO = new Book("Kunkun Paluu", "JumskiLAVIS", "12345", 10);
+                            LOTRO.PrintThings();
+
+                            Console.WriteLine("---------------------------------------------------------");
+
+                            Book NARNIA = new Book("Keittokirja", "Soppamies", "11111", 5);
+                            NARNIA.PrintThings();
+                            Console.WriteLine("---------------------------------------------------------");
+                            Console.WriteLine(LOTRO.CompareBook(NARNIA));
+                            Console.WriteLine("---------------------------------------------------------");
+                            break;
+                        }
+                        
+
+                }
+                
+            } while (choise.ToUpper() != "X");
+
+            string UserInterface()
+            {
+                Console.WriteLine("\n Paina [Q] jos haluat listan kirjoista.");
+                Console.WriteLine("\n Paina [W] jos haluat vertailla kirjojen hintoja.");
+                Console.WriteLine("\n Paina [X] jos haluat sulkea ohjelman.");
+                return Console.ReadLine();
+            }
+        }
+    }
+}
