fd343d80b3dda8c4d80bc900aaca15bc53f77b3e
BobGeews <ossi.hakkinen@hotmail.fi>
2019-01-17 13:32:31 +0200

    Employee app ready

---
+++ EmployeeOlio/EmployeeOlio/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeOlio
+{
+    class Employee
+    {
+        public string name;
+        public int id;
+        public string position;
+        public double salary;
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+
+        }
+        public string CompareSalary(Employee employee)
+        {
+            string text = "";
+            if (this.salary > employee.salary)
+            {
+               text = ($"Työntekijä {this.name} palkka ({this.salary}e) on suurempi kuin työntekijä {employee.name} palkka ({employee.salary}e).");
+            }
+            else
+            {
+                text = ($"Työntekijä {employee.name} palkka ({employee.salary}e) on suurempi kuin työntekijä {this.name} palkka ({this.salary}e).");
+            }
+            return text;
+        }
+        public string PrintEmployeeInfo()
+        {
+            string info = this.name +" "+ this.id +" "+ this.position +" "+ this.salary;
+            return info;
+            
+        }
+        
+    }
+}
---
+++ EmployeeOlio/EmployeeOlio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EmployeeOlio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**TYÖNTEKIJÄ TEHTÄVÄ**");
+            Employee matti = new Employee("Matti Ollikainen",1111,"Toimitusjohtaja",7500);
+            Employee pekka = new Employee("Pekka Pakurinen",1112,"Varatoimitusjohtaja",7000);
+            Employee jukka = new Employee("Jukka Jukarainen",1113,"Tuotantopäällikkö",6000);
+
+
+            string choise = null;
+            do
+            {
+                choise = UserInterface();
+                switch (choise.ToUpper())
+                {
+                    case "Q":
+
+                        Console.WriteLine(pekka.CompareSalary(matti));
+                        Console.WriteLine(pekka.CompareSalary(jukka));
+                        Console.WriteLine(jukka.CompareSalary(matti));
+
+                            break;
+                        
+
+                    case "W":
+                        {
+                            Console.WriteLine( matti.PrintEmployeeInfo());
+                            Console.WriteLine(pekka.PrintEmployeeInfo());
+                            Console.WriteLine(jukka.PrintEmployeeInfo());
+                            break;
+                        }
+                }
+
+            } while (choise.ToUpper() != "X");
+
+            string UserInterface()
+            {
+                Console.WriteLine("\n Vertaile palkkoja: Paina [Q]  ");
+                Console.WriteLine("\n Tulosta lista työntekijöistä: Paina [W] ");
+                Console.WriteLine("\n Sulje sovellus: Paina [X] ");
+                return Console.ReadLine();
+            }
+            
+        }
+    }
+}
