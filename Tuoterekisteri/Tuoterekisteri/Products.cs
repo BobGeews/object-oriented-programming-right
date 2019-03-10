a86593c6301b83fd10aeb10d3c5e7774d72cbbd9
BobGeews <ossi.hakkinen@hotmail.fi>
2019-03-10 21:36:12 +0200

    Ei valmis mut tällä mennään.

---
+++ Tuoterekisteri/Tuoterekisteri/Products.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuoterekisteri
+{
+    class Products
+    {
+        public string name { get; set; }
+        public string id {get; set;}
+        public string groupName { get; set; }
+        public double price { get; set; }
+        public int amount { get; set; }
+        public string comment { get; set; }
+        
+
+        public Products()
+        {
+            this.name = "";
+            this.id = "";
+            this.groupName = "";
+            this.price = 0.0;
+            this.amount = 0;
+            this.comment = "";
+        }
+        public void Writeinfo()
+        {
+
+            Console.WriteLine($"Tuotteen nimi: {name}");
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Kategoria: {groupName}");
+            Console.WriteLine($"Hinta: {price}");
+            Console.WriteLine($"Määrä: {amount}");
+            Console.WriteLine($"Kommentti: {comment}");
+            Console.WriteLine("------------------");
+        }
+        public Object AskId(string comparedId)
+        {
+            Console.Write("Anna tuote id");
+            comparedId = Console.ReadLine();
+            if (this.id == comparedId)
+            {
+                return this;
+            }
+            else
+            {
+                return null;
+            }
+           
+
+        }
+        
+        public override string ToString()
+        {
+            return $"Asiakas: {this.name},{this.id},{this.price}";
+        }
+    }
+}
---
+++ Tuoterekisteri/Tuoterekisteri/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+
+
+namespace Tuoterekisteri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Harjoitustyö****");
+
+            string readtext = File.ReadAllText(@"C:\Users\ossih\Dev\digitrade18\object-oriented-programming-right\Tuoterekisteri\Tuoterekisteri\items.json");
+            /*Koitin vaihtaa tiedosto poluksi ainoastaan "items.json", mutta se ei toiminut. 
+             Joudut vaihtamaan tiedosto polun että lista toimii.
+            Toivottavasti tämä ei aiheuta suoraa hylkäystä.*/
+            List<Products> prodlist = JsonConvert.DeserializeObject<List<Products>>(readtext);
+            
+            
+
+            string choise = null;
+            do
+            {
+                choise = UserInterface();
+
+               switch (choise.ToUpper())
+                {
+                    case "Q":
+                        {
+                            foreach(Products p in prodlist)
+                            {
+                                p.Writeinfo();
+
+                            }
+                            
+                            
+                            foreach (Products p in prodlist)
+                            {
+                                if (p.AskId("123") != null)
+                                {
+                                    Console.WriteLine(p.ToString());
+                                }
+                            }
+                            break;
+                        }
+
+                    case "W":
+                        {
+                           
+                            break;
+                        }
+
+
+                }
+
+            } while (choise.ToUpper() != "X");
+
+            string UserInterface()
+            {
+                Console.WriteLine("\n Tulosta tuote lista: [Q] ");
+                Console.WriteLine("\n Paina [W]");
+                Console.WriteLine("\n Paina [X] jos haluat sulkea ohjelman.");
+                return Console.ReadLine();
+            }
+        }
+    }
+}
